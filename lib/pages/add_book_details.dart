39f5f40fb2889c506727f1b1859aa3f792cc8b83
saiusesgithub <saisrujantesting@gmail.com>
2025-08-25 19:56:11 +0530

    fixed bug where date is being returned as integer and also reorganized code

--- lib/pages/add_book_details.dart
+++ lib/pages/add_book_details.dart
@@ -17,9 +17,14 @@ class _AddBookDetailsState extends State<AddBookDetails> {
     _bookData = getBookDataByIsbn(widget.isbn);
   }
 
+  final List<DropdownMenuItem<String>> _items =
+      ['To-Read', 'Reading', 'Read', 'Dropped'].map((String value) {
+        return DropdownMenuItem<String>(child: Text(value), value: value);
+      }).toList();
+  String _selectedStatus = 'To-Read';
+
   @override
   Widget build(BuildContext context) {
-    String _isbn = widget.isbn;
     return Scaffold(
       body: Container(
         child: FutureBuilder(
@@ -33,8 +38,8 @@ class _AddBookDetailsState extends State<AddBookDetails> {
               final _data = snapshot.data;
               String title = _data?['title'] ?? 'Unknown Title';
               String publishedDate =
-                  (_data?['publishedDate'] ?? 'Unknown Published Date')
-                      .toString();
+                  (_data?['publishedDate'].toString() ??
+                  'Unknown Published Date');
               String pageCount = (_data?['pageCount'] ?? 'Unknown Page Count')
                   .toString();
 
@@ -46,28 +51,100 @@ class _AddBookDetailsState extends State<AddBookDetails> {
               }
               String author = _data?['author'] ?? 'Unknown Author';
 
-              return Column(
-                children: [
-                  SizedBox(
-                    height: 200,
-                    width: double.infinity,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(image: NetworkImage(imageLink)),
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      width: double.infinity,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(imageLink),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  Text(title),
-                  Text(author),
-                  Text(publishedDate),
-                  Text(pageCount),
-                ],
+                    _bookTitle(title),
+                    SizedBox(height: 50),
+                    _bookAuthor(author),
+                    _bookPublishedDate(publishedDate),
+                    _bookPageCount(pageCount),
+                    _statusDropDown(),
+                  ],
+                ),
               );
             } else {
               return const Center(child: Text("No data found or error"));
             }
           },
         ),
+      ),
+    );
+  }
+
+  Widget _bookTitle(String title) {
+    return Center(
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 25,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  Widget _bookAuthor(String author) {
+    return Text(
+      'by $author',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _bookPublishedDate(String date) {
+    return Text(
+      'Date Published - $date',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _bookPageCount(String pages) {
+    return Text(
+      'Page Count - $pages',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _statusDropDown() {
+    return Container(
+      child: DropdownButton<String>(
+        underline: Container(),
+        items: _items,
+        value: _selectedStatus,
+        dropdownColor: Color.fromRGBO(53, 53, 53, 1.0),
+        style: TextStyle(color: Colors.white),
+        onChanged: (value) {
+          setState(() {
+            _selectedStatus = value!;
+          });
+        },
       ),
     );
   }
